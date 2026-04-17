c3140302634f6c3e260a368c4ac13b072bd54f4a
shishi-hiro <dhdjksudb@gmail.com>
2026-04-17 22:02:40 +0900

    Add multiple tasks feature

--- TodoApp.java
+++ TodoApp.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TodoApp {
@@ -5,12 +6,21 @@ public class TodoApp {
 
         Scanner scanner = new Scanner(System.in);
 
-        String task;
+        // 複数のタスクを保存するリスト
+        ArrayList<String> tasks = new ArrayList<>();
 
-        System.out.print("タスクを入力してください：");
-        task = scanner.nextLine();
+        // タスクを2回入力
+        for (int i = 0; i < 2; i++) {
+            System.out.print("タスクを入力してください：");
+            String task = scanner.nextLine();
+            tasks.add(task);
+        }
 
-        System.out.println("あなたのタスクは：" + task);
+        // 一覧表示
+        System.out.println("\nタスク一覧：");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ": " + tasks.get(i));
+        }
 
         scanner.close();
     }
